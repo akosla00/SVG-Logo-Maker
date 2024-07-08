3951efc6108c69d455da797659ab32cdfd76def5
akosla00 <akosla00@gmail.com>
2024-07-08 17:46:45 -0400

    commit

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const { Square, Circle, Triangle } = require('./lib/shapes.js');
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, shapes(data), (err) =>
+        err ? console.log(err) : console.log('Successfully created logo.svg!')
+    );
+}
+
+function shapes(data) {
+    if (data.shape === 'Square') {
+        const square = new Square(data);
+        return square.renderSquare(data);
+    }
+    if (data.shape === 'Circle') {
+        const circle = new Circle(data);
+        return circle.renderCircle(data);
+    }
+    if (data.shape === 'Triangle') {
+        const triangle = new Triangle(data);
+        return triangle.renderTriangle(data);
+    }
+}
 
 inquirer
     .prompt([
@@ -23,4 +46,6 @@ inquirer
             name: 'shapeColor',
             message: 'Enter a color (or hexidecimal number',
         },
-    ])
+    ]).then((answers) => {
+        writeToFile('./examples/logo.svg', answers);
+    });
